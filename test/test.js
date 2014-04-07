39105ca2fe0d3afc4ee15775d271f7d10ecd48c6
mercury landed <wsk3201@gmail.com>
2014-04-07 21:57:53 +0800

    first commit

--- test/test.js
+++ test/test.js
@@ -1,30 +1,11 @@
-Wrapper
-=======
+/**
+ * Created by mercury on 14-4-5.
+ */
 
-Object event Wrapper
-
----
-
-Wrapped raw object with a EventEmitter.You can use wrapped object as a raw object.
-
-Wrap object
-------
-
-```javascript
-
+var _ = require('lodash');
 var wrap = require('../wrapper').Wrap;
-
 var a = wrap({a: 1, b: {c: 3, d: 4}});
-
 console.log(a._sub, a._raw);
-
-```
-
-
-Listen change event
-------
-
-```javascript
 
 a.b.on('change:c', function (v,pv) {
   console.log('change can value: %s previous value: %s',v,pv);
@@ -41,5 +22,3 @@ a.b = 1;
 a.b = [1, 2, 3];
 
 console.log(a._sub, a._raw);
-
-```
